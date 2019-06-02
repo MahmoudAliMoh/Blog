d5173cde23fcfc6b5540fb3926fb75f44b0974c4
MahmoudAliMoh <mahmoudali5@outlook.com>
2019-06-02 18:34:48 +0200

    create blog comments vie

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,8 +8,7 @@
                 <div class="card">
                     <img src="{{ asset('storage/'. $item['cover']) }}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <a href="{{ route('home.show', $item['id']) }}"><h5 class="card-title">{{ $item['title'] }}</h5>
-                        </a>
+                        <h5 class="card-title">{{ $item['title'] }}</h5>
                         <p class="card-text">
                             {{ $item['content'] }}
                         </p>
@@ -18,6 +17,49 @@
                         <small class="text-muted">Category: {{ $item['categories']['data']['name'] }}</small>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="media" style="width: 70%; margin-top: 30px;">
+                <div class="media-body">
+                    <h5 class="mt-0">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+                    ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+            </div>
+
+            <div class="media" style="width: 70%; margin-top: 30px;">
+                <div class="media-body">
+                    <h5 class="mt-0">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+                    ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+            </div>
+            <div class="media" style="width: 70%; margin-top: 30px;">
+                <div class="media-body">
+                    <h5 class="mt-0">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+                    ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form>
+                    <div class="form-group">
+                        <label for="comment">Email address</label>
+                        <textarea name="comment" id="comment" class="form-control"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Comment</button>
+                </form>
             </div>
         </div>
     </div>
