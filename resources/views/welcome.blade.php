b8240eab77e6359736f1b46022ca62bae3d6aee8
MahmoudAliMoh <mahmoudali5@outlook.com>
2019-06-03 05:19:02 +0200

    edit blog style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,18 @@
         <div class="row justify-content-center">
             <div class="card-deck" style="width: 75%">
                 @foreach($blog as $item)
-                    <div class="card">
-                        <img src="{{ asset('storage/'. $item['cover']) }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <a href="{{ route('home.show', $item['id']) }}"><h5 class="card-title">{{ $item['title'] }}</h5></a>
-                            <p class="card-text">
-                                {{ str_limit($item['content'], $limit = 150, $end = '...') }}
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Category: {{ $item['categories']['data']['name'] }}</small>
+                    <div class="col-md-12">
+                        <div class="card">
+                            <img src="{{ asset('storage/'. $item['cover']) }}" class="card-img-top">
+                            <div class="card-body">
+                                <a href="{{ route('home.show', $item['id']) }}"><h5 class="card-title">{{ $item['title'] }}</h5></a>
+                                <p class="card-text">
+                                    {{ str_limit($item['content'], $limit = 150, $end = '...') }}
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Category: {{ $item['categories']['data']['name'] }}</small>
+                            </div>
                         </div>
                     </div>
                 @endforeach
